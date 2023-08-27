7d37d25b124dd25cdf7d6a20fef6a444794d7532
GatoBinario <43298345+GatoBinario@users.noreply.github.com>
2023-08-27 21:53:09 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let games = [
+    {
+        userName: "BrawlUser1234",
+        userID: "#12345678",
+        req_throphy: 32,
+        gamemode: "brawlball",
+    }
+]
+
+let bod = document.querySelector("body");
+
+games.forEach(game => { 
+    let game = document.createElement("div")
+    
+    //bod.append(game)
+});
